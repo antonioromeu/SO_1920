3b789b56d98297d3e06dcf2aad9a602d78328c88
lucet15 <lucialucet@gmail.com>
2019-11-08 19:03:15 +0000

    runTests

---
+++ runTests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+inputdir="${1}"
+outputdir="${2}"
+numthreads="${3}"
+numbuckets="${4}"
+
+[[ -d $outputdir ]] || mkdir ${outputdir}
+for test in $(ls ${inputdir}/*.txt)
+do
+    testname=${test#*/}
+    echo InputFile=${test#*/} NumThreads=1
+    ./tecnicofs-nosync ${inputdir}/${testname} ${outputdir}/${testname%%.*}-1.txt 1 1 | grep -h "TecnicoFS completed in "
+    for threads in $(seq 2 ${numthreads})
+    do
+        echo InputFile=${test#*/} NumThreads=${threads}
+        ./tecnicofs-mutex ${inputdir}/${testname} ${outputdir}/${testname%%.*}-${threads}.txt ${threads} ${numbuckets} | grep -h "TecnicoFS completed in "
+    done
+done
