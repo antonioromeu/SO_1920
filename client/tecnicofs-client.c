07de17a3938098180cb9f9c996ca17d3af362652
antonioromeu <antonioromeu@tecnico.ulisboa.pt>
2019-11-20 12:07:51 +0000

    server started

--- client/tecnicofs-client.c
+++ client/tecnicofs-client.c
@@ -2,14 +2,17 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
+int socket;
+
 int tfsMount(char* address) {
-    int sockfd;
+    int sockfd, dim_serv;
     struct sockaddr_un end_serv;
     if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
         perror("Erro ao criar socket clinte");
+    socket = sockfd;
     bzero((char*) &end_serv, sizeof(end_serv));
     end_serv.sun_family = AF_UNIX;
-    strcpy(end_serv.sun_path, "/tmp/socket.unix.stream");
+    strcpy(end_serv.sun_path, address);
     dim_serv = strlen(end_serv.sun_path) + sizeof(end_serv.sun_family);
     if (connect(sockfd, (struct sockaddr*) &end_serv, dim_serv) < 0)
         perror("Error ao fazer connect no client");
@@ -17,4 +20,6 @@ int tfsMount(char* address) {
 }
 
 int tfsUnmout() {
-    
+    close(socket);
+    exit(0);
+}
